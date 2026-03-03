295aee1de4fdfbeabcaa05eae03086899682b55f
Yamazaki <yamazaki@eleven-ws.com>
2026-03-03 16:46:42 +0900

    fix: set relative base path and update homepage url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/hsflow/', // for GH pages
+    base: './', // for GH pages
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
